b83d67acdaabbb703fc524baac1f031e4a105df2
himahardeepdev <himahardeep@gmail.com>
2024-05-07 12:58:09 +0530

    correction of todolist warings

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { dataStore } from '../globalScopeData/GlobalData'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
